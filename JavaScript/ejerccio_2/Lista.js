b7151a5c5a61435b24df538afb38bd280e4b2961
jmrogero <115324295+jmrogero@users.noreply.github.com>
2022-10-14 16:00:01 +0200

    Update Lista.js

---
+++ JavaScript/ejerccio_2/Lista.js
@@ -0,0 +1,16 @@
+const miLibro = {
+    titulo: "El guardian entre el centeno",
+    autor: "J. D. Salinge",
+    fecha: ("1951"),
+    url: "https://es.wikipedia.org/wiki/The_Catcher_in_the_Rye"
+}
+
+const fechaNacimiento = new Date("Feb 16, 1984");
+
+const lista = ["Jose Maria", 38, false, fechaNacimiento, miLibro];
+
+console.log(lista[0])
+console.log(lista[1])
+console.log(lista[2])
+console.log(lista[3])
+console.log(lista[4])
