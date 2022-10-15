fcf331f9bcf41464945dcea557def62ac995728a
jmrogero <115324295+jmrogero@users.noreply.github.com>
2022-10-15 17:31:35 +0200

    numeros

---
+++ JavaScript/ejetccio_5/Numeros.js
@@ -0,0 +1,20 @@
+//- Una variable que contenga tu altura en centímetros (entero)
+let alturaCm = 178;
+console.log(alturaCm);
+
+//- Una variable que contenga tu altura en metros (número de coma flotante)
+let alturaM = 1.78;
+console.log(alturaM);
+
+//- Una variable que contenga tu peso en kilogramos (número de coma flotante)
+let pesoKg = 75.4;
+console.log(pesoKg);
+
+//- Una variable que contenga tu altura en metros redondeada hacia arriba
+
+//- Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+let pesoKgRedondeoBajo = Math.round(pesoKg);
+console.log(pesoKgRedondeoBajo);
+
+
+//- Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
