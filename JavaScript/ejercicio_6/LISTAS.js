552354f91e6e1a6ad7c50f9698024cc956d4c1d8
jmrogero <115324295+jmrogero@users.noreply.github.com>
2022-10-16 16:04:18 +0200

    EJERCICIOS

---
+++ JavaScript/ejercicio_6/LISTAS.js
@@ -0,0 +1,23 @@
+//- Una variable que contenga la lista de la compra (mínimo 5 elementos)
+let listaCompra = ["leche","pan","yogurt","cafe","tomate"];
+console.log(listaCompra);
+
+//- Modifica la lista de la compra y añádele "Aceite de Girasol"
+listaCompra.push("Aceite de Girasol");
+console.log(listaCompra);
+
+//- Vuelve a modificar la lista de la compra eliminando "Aceite de Girasol"
+listaCompra.pop();
+console.log(listaCompra);
+
+//- Una lista de tus 3 películas favoritas (objetos con propiedades: titulo, director, fecha)
+
+//- Una nueva lista que contenga las películas posteriores al 1 de enero de 2010 (utilizando filter)
+
+//- Una nueva lista que contenga los directores de la lista de películas original (utilizando map)
+
+//- Una nueva lista que contenga los títulos de la lista de películas original (utilizando map)
+
+//- Una nueva lista que concatene la lista de directores y la lista de los títulos (utilizando concat)
+
+//- Una nueva lista que concatene la lista de directores y la lista de los títulos (utilizando el factor de propagación)
