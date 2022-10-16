03527cf18a64d8bf6d1c58f00d10733aa148d94d
jmrogero <115324295+jmrogero@users.noreply.github.com>
2022-10-16 15:32:42 +0200

    numeros

--- JavaScript/ejetccio_5/Numeros.js
+++ JavaScript/ejetccio_5/Numeros.js
@@ -1,22 +1,24 @@
-//- Una variable que contenga tu altura en centímetros (entero)
+//Una variable que contenga tu altura en centímetros (entero)
 let alturaCm = 178;
 console.log(alturaCm);
 
-//- Una variable que contenga tu altura en metros (número de coma flotante)
+//Una variable que contenga tu altura en metros (número de coma flotante)
 let alturaM = 1.78;
 console.log(alturaM);
 
-//- Una variable que contenga tu peso en kilogramos (número de coma flotante)
+//Una variable que contenga tu peso en kilogramos (número de coma flotante)
 let pesoKg = 75.4;
 console.log(pesoKg);
 
-//- Una variable que contenga tu altura en metros redondeada hacia arriba
+//Una variable que contenga tu altura en metros redondeada hacia arriba
 let alturaMRedondeoAlto = Math.ceil(alturaM);
 console.log(alturaMRedondeoAlto);
 
-//- Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+//Una variable que contenga tu peso en kilogramos redondeado hacia abajo
 let pesoKgRedondeoBajo = Math.floor(pesoKg);
 console.log(pesoKgRedondeoBajo);
 
-
-//- Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+//Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+let maximoValor = (Number.MAX_VALUE) ;
+let maximoValorMasUno = (maximoValor == (maximoValor +1));
+console.log(maximoValorMasUno);
