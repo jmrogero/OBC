314d5d639a73bf991c919bd17572805e8771d64b
jmrogero <115324295+jmrogero@users.noreply.github.com>
2022-10-19 23:59:25 +0200

    EJERCICIOS

---
+++ JavaScript/ejercicio_8/funciones.js
@@ -0,0 +1,16 @@
+// Crea un archivo JS que contenga las siguientes líneas
+
+// - Una función sin parámetros que devuelva siempre "true"
+function siempreTrue() {
+    return true;
+  }
+  const devuelveTrue = siempreTrue()
+  console.log(devuelveTrue)
+  
+  // - Una función asíncrona que utilice un setTimeout y pase por consola un "Hola soy una promesa" 5 segundos después de haberse ejecutado
+  function asincrona() {
+      return true;
+    }
+  
+  // - Una función generadora de índices pares automáticos
+  
