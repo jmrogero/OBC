5abf7453b19ebcf1c7d4c8031b33ee18d14fecad
jmrogero <115324295+jmrogero@users.noreply.github.com>
2022-10-20 23:56:43 +0200

    EJERCICIOS

--- JavaScript/ejercicio_8/funciones.js
+++ JavaScript/ejercicio_8/funciones.js
@@ -1,16 +1,32 @@
 // Crea un archivo JS que contenga las siguientes líneas
 
-// - Una función sin parámetros que devuelva siempre "true"
+// Una función sin parámetros que devuelva siempre "true"
 function siempreTrue() {
-    return true;
-  }
-  const devuelveTrue = siempreTrue()
-  console.log(devuelveTrue)
-  
-  // - Una función asíncrona que utilice un setTimeout y pase por consola un "Hola soy una promesa" 5 segundos después de haberse ejecutado
-  function asincrona() {
-      return ;
+  return true;
+}
+const devuelveTrue = siempreTrue();
+console.log(devuelveTrue);
+
+// Una función asíncrona que utilice un setTimeout y pase por consola un "Hola soy una promesa" 5 segundos después de haberse ejecutado
+function asíncrona() {
+  console.log("Hola soy una promesa");
+}
+setTimeout(asíncrona, 5000);
+
+// Una función generadora de índices pares automáticos
+function* generaIndicesPares() {
+  let i = 0;
+  while (true) {
+    i = i + 2;
+    if (i > 10) {
+      return i;
     }
-    console.log(devuelveTrue)
-  // - Una función generadora de índices pares automáticos
-  
+    yield i;
+  }
+}
+const gen = generaIndicesPares();
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
