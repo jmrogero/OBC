0151a44866c799176e8f8c4e2e0c2c2c3405feb9
jmrogero <115324295+jmrogero@users.noreply.github.com>
2022-10-22 00:10:24 +0200

    EJERCICIO

---
+++ JavaScript/ejercicio_9/index.js
@@ -0,0 +1,29 @@
+
+const num = 1;
+try{
+num === 2;
+console.log("Este numero es igual a 2")
+}
+catch (e) {
+    console.log("Este numero no es igual a 2")
+    registrador
+    
+
+    
+}
+
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
