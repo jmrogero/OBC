5008d1e72c24ed681dbcc6ed558fe365c5ce3ee5
jmrogero <115324295+jmrogero@users.noreply.github.com>
2022-10-26 10:25:07 +0200

    EJERCICIOS

--- JavaScript/ejercicio_11/clases.js
+++ JavaScript/ejercicio_11/clases.js
@@ -4,9 +4,10 @@ class estudiante {
         this.asignaturas = asignaturas
         }
     obtenDatos(){
-        console.log(`El nombre del estudiante es ${this.nombre} y sus asignaturas son ${this.asignaturas}`)
+        return (`El nombre del estudiante es ${this.nombre} y sus asignaturas son ${this.asignaturas}`)
     }
 }
 
 const estudiante_1 = new estudiante ("Chema", ["Javascript", "HTML", "CSS"])
 estudiante_1.obtenDatos()
+console.log(estudiante_1.obtenDatos())
