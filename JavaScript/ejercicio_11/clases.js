be0f3a431a6cb4db38fbc66af0155e5dcc13ac74
jmrogero <115324295+jmrogero@users.noreply.github.com>
2022-10-26 10:10:53 +0200

    EJERCICIOS

---
+++ JavaScript/ejercicio_11/clases.js
@@ -0,0 +1,12 @@
+class estudiante {
+    constructor(nombre, asignaturas){
+        this.nombre = nombre
+        this.asignaturas = asignaturas
+        }
+    obtenDatos(){
+        console.log(`El nombre del estudiante es ${this.nombre} y sus asignaturas son ${this.asignaturas}`)
+    }
+}
+
+const estudiante_1 = new estudiante ("Chema", ["Javascript", "HTML", "CSS"])
+estudiante_1.obtenDatos()
